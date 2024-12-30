e4d3ac3deaf9d1d13ecbad5858bf98809413767f
liwjak <liwjak@digitalchina.com>
2024-12-30 15:40:48 +0800

    单元测试调整

--- src/test/java/com/douyin/catchbarragepublic/CatchBarragePublicApplicationTests.java
+++ src/test/java/com/douyin/catchbarragepublic/CatchBarragePublicApplicationTests.java
@@ -2,13 +2,17 @@ package com.douyin.catchbarragepublic;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class CatchBarragePublicApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CatchBarragePublicApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CatchBarragePublicApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		System.out.println("123");
 	}
 
 }
